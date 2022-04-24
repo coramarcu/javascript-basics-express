0e6fa6b52ff28af7963913285c6e796c963975c1
Cora Marcu <cora.marcu@gmail.com>
2022-04-24 20:42:10 +0100

    pass  subtract tests

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const {
   firstCharacter,
   firstCharacters,
 } = require('./lib/strings');
+const { add, subtract } = require('./lib/numbers');
 
 const app = express();
 
@@ -29,6 +30,32 @@ app.get('/strings/first-characters/:string', (req, res) => {
     res.send({ result: firstCharacters(req.params.string, req.query.length) });
   }
   res.send({ result: firstCharacter(req.params.string) });
+});
+
+app.get('/numbers/add/:num1/and/:num2', (req, res) => {
+  const { num1, num2 } = req.params;
+  const number1 = parseInt(num1, 10);
+  const number2 = parseInt(num2, 10);
+  if (Number.isNaN(number1) || Number.isNaN(number2)) {
+    res.status(400);
+    res.send({ error: 'Parameters must be valid numbers.' });
+  }
+  res.status(200);
+  res.send({ result: add(number1, number2) });
+});
+
+app.get('/numbers/subtract/:number1/from/:number2', (req, res) => {
+  const { number1, number2 } = req.params;
+  const num1 = parseInt(number1, 10);
+  const num2 = parseInt(number2, 10);
+
+  if (Number.isNaN(num1) || Number.isNaN(num2)) {
+    res.status(400);
+    res.send({ error: 'Parameters must be valid numbers.' });
+  }
+
+  res.status(200);
+  res.send({ result: subtract(num2, num1) });
 });
 
 module.exports = app;
